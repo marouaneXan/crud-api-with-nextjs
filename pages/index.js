aab4a232b179566e75879d206e467601c2d97521
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-04 23:10:05 +0100

    display all data in frontend in table

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,26 @@ const Home = () => {
     getPosts();
   },[]);
   return (
-    <div>
-      <h1>All Posts</h1>
+    <div className="container">
+      <h1>There are {posts.length} in the database</h1>
+      <table className="table">
+        <thead>
+          <th>Title</th>
+          <th>Update</th>
+          <th>Delete</th>
+        </thead>
+        <tbody>
+          {
+            posts.map(post=>
+                <tr>
+                  <td>{post.title}</td>
+                  <td><button className="btn btn-warning">Update</button></td>
+                  <td><button className="btn btn-danger">Delete</button></td>
+                </tr>
+            )
+          }
+        </tbody>
+      </table>
     </div>
   );
 };
